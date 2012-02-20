bb8764917f4c8c6c8b7fa3c3acbbc1eb49fbcf17
Ben Root <ben.v.root@gmail.com>
2012-02-20 15:09:25 -0600

    Added "Optimal Trackers" param file and a shell command to plot the analyses from those trackers.

---
+++ comparison.sh
@@ -0,0 +1,16 @@
+MultiScenarioAnalysis.py CleanSquall{Sans,Over}Clutter \
+                         DirtySquall{Sans,Over}Clutter \
+                         {Slow,Fast}Storms \
+                         {Lo,Med,Hi}Density \
+                         SquallLine1 SquallLine2 \
+                   -s PC -t SCIT TITAN MHT --titles "Tracker Comparison - PC" \
+                   --ticklabels "`echo -e 'Clean w/o\nClutter'`" \
+                   "`echo -e 'Clean w/\nClutter'`" \
+                   "`echo -e 'Noisy w/o\nClutter'`" \
+                   "`echo -e 'Noisy w/\nClutter'`" \
+                   Slow Fast Low Medium High Parallel Perpendicular \
+                   --plotlabels SCIT TITAN MHT --mode bar \
+                   --xlabel Scenarios \
+                   --filter Tracks+ Splitter+ Merger+ Spurious+ \
+                   -f 14 4.5 \
+                   --noshow --save TrackerCompare --type eps --bw
